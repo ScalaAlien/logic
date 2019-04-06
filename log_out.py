17a6f9444b8f8fa4610b2d4276f66cdd9ebb7a01
ScalaAlien <scala_alien@gmail.com>
2019-04-06 12:16:56 +0900

    Add comment

--- log_out.py
+++ log_out.py
@@ -1,19 +1,14 @@
-from sympy.logic import *
-from sympy import S
+"""This module -- Simplify logic program."""
+
 from logging import *
 from sys import *
 
 
 def log_out(name, out):
+    """log_out -- log output function."""
     logger = getLogger(name)
     logger.setLevel(DEBUG)
     logger.addHandler(FileHandler('%s.log' % name))
     logger.addHandler(StreamHandler(stdout))
     logger.debug(out)
     return logger
-
-
-def logic_out(arg):
-    log_out('input', S(arg))
-    log_out('dnf', simplify_logic(arg, form='dnf'))
-    log_out('cnf', simplify_logic(arg, form='cnf'))
--- simplify.py
+++ simplify.py
@@ -1,5 +1,16 @@
-from logic_out import *
+"""This module -- Simplify logic program."""
+from log_out import *
 from sympy.abc import a, b, c, d, e, f
+from sympy.logic import *
+from sympy import S
+
+
+def logic_out(arg):
+    """logic_out -- logic output function."""
+    log_out('input', S(arg))
+    log_out('dnf', simplify_logic(arg, form='dnf'))
+    log_out('cnf', simplify_logic(arg, form='cnf'))
+
 
 logic = ((a & b) | ((a | c) & d) & e)
 logic_out(logic)
